fa65a565c609c676ccac3212cd0ce139507e5cb8
danieladebanjo55 <supremenftcoder@gmail.com>
2024-07-13 11:32:26 +0100

    docs: calculator program upload

--- WEEK-1/calculator.py
+++ WEEK-1/calculator.py
@@ -1,1 +1,21 @@
-# Arithmetic, Comparison, Logical Operators
+# Simple calculator program
+
+print("Hippie's Basic Calculator Program!")
+
+a = int(input("Enter integer value for a: ")) 
+b = int(input("Enter integer value for b: "))
+operator = input("Enter an operator: e.g *, +, -, /: ")
+
+if operator == '-':
+    result = a - b
+elif operator == '+':
+    result = a + b
+elif operator == '*':
+    result = a * b
+elif operator == '/':
+    result = a / b
+else:
+    print("Please Enter a Valid OPerator!")
+
+print(f"{a} {operator} {b} = {result}")
+# print(result)
